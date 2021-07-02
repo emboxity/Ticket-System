05273cda73b71c868d4ad412f88d3e7a3da66663
Embox <71951742+emboxity@users.noreply.github.com>
2021-07-01 22:46:33 -0400

    Add files via upload

---
+++ commands/about.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const pfp = 'https://i.imgur.com/zEcnBhv.png';
+module.exports = {
+    name: 'about',
+    description: 'Gives information about the bot.',
+    cooldown: 1.5,
+    guildOnly: true,
+    run: async(client, message, args) =>{
+
+        const target = message.mentions.members.first()
+
+        if(target || args[0]){
+            const ErrorLOL = new Discord.MessageEmbed()
+            .setDescription("\<:x_:842221675775787019> The command failed")
+            .setColor("#ff6961")
+            setFooter('Trade Hangout * made by Squshu',client.user.displayAvatarURL())
+            return message.channel.send(ErrorLOL)
+        }
+
+        const embed = new Discord.MessageEmbed()
+        .setAuthor('Trade Hangout', pfp)
+        .setDescription('The bot is coded in JavaScript.')
+
+        .addFields(
+            { name: 'Creator', value: 'Squshu' },
+            { name: 'Member Count', value: `${client.guilds.cache.size}`},
+        )
+        .addField('Github', 'https://github.com/emboxity/embox-bot')
+        .addField('Source Code', 'https://github.com/emboxity/Trade-Hangoutt', true)
+        .setFooter('Trade Hangout * made by Squshu',client.user.displayAvatarURL())
+
+        message.channel.send(embed)
+
+
+
+        
+        }}
