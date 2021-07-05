57052107613b9e995f43dd3b07b09ff313dc03fe
Embox <71951742+emboxity@users.noreply.github.com>
2021-07-05 19:17:34 -0400

    Fixed Errors

--- commands/createTranscript.js
+++ commands/createTranscript.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const sourcebin = require('sourcebin');
+const sourcebin = require('sourcebin_js');
 
 module.exports = {
     name: 'transcript',
@@ -8,32 +8,27 @@ module.exports = {
     guildOnly: true,
     run: async (client, message, args) => {
         const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.channel;
-		if (channel.name.includes('ticket-')) {
-			if (channel.name === `ticket-${message.author.tag}`) {
+			if (channel.name === `ticket-${message.author.id}`) {
                 channel.messages.fetch().then(async (messages) => {
 					const TicketTranscript = messages.array().reverse().map(m => `${new Date(m.createdAt).toLocaleString('en-US')} - ${m.author.tag}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).join('\n');
-                    try{
-                    const transcript = await sourcebin.create(
-                        [
-                            {   
-                                name: '',
-                                content: TicketTranscript,
-                                language: 'text',
-                            },
-                        ],
-                        {
-                            title: `Transcript for ${channel.name}`,
-                            description: '',
-                        },
-                    );
-                } catch(error){
-                    return message.channel.send('An error occurred while trying to fetch the transcript.');
-                }
-                const embed = new Discord.MessageEmbed()
-                    .setDescription(`[\`Transcript\`](${TicketTranscript.url})`)
-					message.channel.send('The transcript has been saved.', embed);
+                    let transcript;
+                    transcript = sourcebin.create([
+                    {
+                        name: '',
+                        content: TicketTranscript,
+                        languageId: 'text'
+                    }
+                ], {
+                    title: `Chat transcript for ${channel.name}`,
+                    description: ' ',
                 })
-            }
+                
+                    .then(transcript => message.reply(`Here is the transcript. ${transcript.url}`))
+                    .catch(console.error);
+            
+					
+                })
+            
         }	else {
 			return;
     
