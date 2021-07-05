1a7e7ccbe5b336ed8a713aad25b59cb2f2c3ef46
Embox <71951742+emboxity@users.noreply.github.com>
2021-07-05 19:11:53 -0400

    updated ticket creation name

--- commands/createTicket.js
+++ commands/createTicket.js
@@ -1,20 +1,18 @@
 const Discord = require('discord.js');
-const db = require('quick.db');
+
 module.exports = {
     name: 'ticket',
     description: "lets you make a ticket",
     cooldown: 2,
     guildOnly: true,
     run: async (client, message, args) => {
-      
-      db.add('times.ticket', 1); 
-      const timesUsed = db.get('times.ticket');
+    
 
-      if(message.guild.channels.cache.find(channel => channel.name === `ticket-${message.author.tag}`)) {
+      if(message.guild.channels.cache.find(channel => channel.name === `ticket-${message.author.id}`)) {
         return message.reply('you already have an existing ticket, please close your ticket first before opening a new one!');
       }
 
-      message.guild.channels.create(`ticket-${message.author.tag}`, {
+      message.guild.channels.create(`ticket-${message.author.id}`, {
         type: 'text',
         permissionOverwrites: [
            {
@@ -33,10 +31,10 @@ module.exports = {
                 
 
               })
-              // .then((channel) => {
-              //   const categoryId = '762077125020614666'
-              //   channel.setParent(categoryId)
-              // })
+              .then((channel) => {
+                const categoryId = '762077125020614666'
+                channel.setParent(categoryId)
+              })
               .then(async channel => {
                 
                 const ticketEmbed = new Discord.MessageEmbed()
