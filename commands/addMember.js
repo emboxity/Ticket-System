4d7091bf369981e83a397d2a807d88d8a011e249
Embox <71951742+emboxity@users.noreply.github.com>
2021-07-08 16:48:40 -0400

    added conditional variables

--- commands/addMember.js
+++ commands/addMember.js
@@ -2,19 +2,29 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'add',
-    description: "adds user to a ticket",
-    cooldown: 2,
+    description: "Adds a user to a ticket",
+    cooldown: 25,
     guildOnly: true,
+    usage: '+add (User Mention) or (User ID)',
     run: async (client, message, args) => {
-        if(message.channel.name.includes('ticket-')) {
-			const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(' ') || x.user.username === args[0]);
-        if(!member){
-                const error = new Discord.MessageEmbed()
-                .setDescription("\<:x_:842221675775787019> The command failed. Did you try to")
-                .setColor("#ff6961")
-                .setFooter('Ticket System * made by shiba#2254',client.user.displayAvatarURL())
-                return message.channel.send(error)
+        const member = message.mentions.members.first();
+        var uID = (!member) ? await message.guild.members.fetch(args[0]) : console.log('No ID detected, so I am looking for a mention.')
+
+        console.log(uID);
+        console.log(member);
+
+        if(!member && !uID){
+            const error = new Discord.MessageEmbed()
+            .setDescription("Invalid user")
+            .setColor("#ff6961")
+            .setFooter('Ticket System * made by shiba#2254',client.user.displayAvatarURL())
+            return message.channel.send(error)
         }
+
+        if(message.channel.name.includes('ticket-') && member && !uID) {
+        
+         		
+
         try {
             message.channel.updateOverwrite(member.user, {
                 VIEW_CHANNEL: true,
@@ -27,5 +37,24 @@ module.exports = {
         } catch (error){
             return message.channel.send('An error occured whilst trying to add someone into the ticket.')
         }
-    }
+    }   else if(message.channel.name.includes('ticket-') && !member && uID) {
+        
+         		
+
+        try {
+            message.channel.updateOverwrite(uID.user, {
+                VIEW_CHANNEL: true,
+                SEND_MESSAGES: true,
+                ATTACH_FILES: true,
+                READ_MESSAGE_HISTORY: true,
+            }).then(() => {
+                message.channel.send(`Successfully added ${uID} to ${message.channel}`);
+            });
+        } catch (error){
+            return message.channel.send('An error occured whilst trying to add someone into the ticket.')
+        }
+    }  else {
+        return;
+    } 
+    
 }}
